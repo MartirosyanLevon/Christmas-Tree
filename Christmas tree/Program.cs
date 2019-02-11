2e50c4ba260a015f553cc79fad1cc5a251e95e2b
Lev# <mr.lmartirosyan888@gmail.com>
2019-02-11 22:23:52 +0400

    relise one

---
+++ Christmas tree/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elochka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you want to draw Christmas tree");
+            Console.WriteLine("write what height you want ?");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = x;
+            int z = 1;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < z; k++)
+                {
+                    Console.Write("*");
+                }
+                y -= 1;
+                z += 2;
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
